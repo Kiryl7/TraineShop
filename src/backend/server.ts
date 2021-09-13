a50462a7a660eb44b61b184fe9c2edb25201a25e
Kiryl7 <>
2021-09-13 18:22:24 +0300

    repair .json files

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -29,8 +29,6 @@ app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req: express.Request, res: express.Response) => res.send(process.env.PAYPAL_CLIENT_ID))
 
-const __dirname = path.resolve()
-
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
